332a8341fae8ac226d48a5795df899829f8b9c1c
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-05 01:59:11 +0100

    added animation loop

---
+++ game.js
@@ -0,0 +1,39 @@
+function new2dCanvas(id, width, height) {
+  const canvas = document.getElementById(id);
+  const ctx = canvas.getContext("2d");
+  canvas.width = width;
+  canvas.height = height;
+  return [canvas, ctx];
+}
+
+const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
+
+const FPS = 60;
+const settings = {
+  fps: FPS,
+  fpsInterval: 1000 / FPS,
+};
+
+let stop = false,
+  now,
+  lastFrame;
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+(function startAnimating() {
+  lastFrame = window.performance.now();
+  animate();
+})();
+
+function animate(newtime) {
+  if (stop) return;
+  requestAnimationFrame(animate);
+  now = newtime;
+  const elapsed = now - lastFrame;
+  if (elapsed > settings.fpsInterval) {
+    lastFrame = now - (elapsed % settings.fpsInterval);
+    update();
+  }
+}
